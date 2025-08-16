dde2f9dc75622003596ebccf839bb2b5d49ea73b
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-16 11:23:52 +0530

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,54 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/footer';  // changed to lowercase 'footer'
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import ServiceDetails from './pages/ServiceDetails';
+
+import Blogs from './pages/Blogs';
+import BlogPost from './pages/BlogPost';
+
+import Signup from './pages/auth/Signup';
+import VerifyOTP from './pages/auth/VerifyOTP';
+import SetPassword from './pages/auth/SetPassword';
+import Login from './pages/auth/Login';
+import ForgotPassword from './pages/auth/ForgotPassword';
+import ResetPasswordOTP from './pages/auth/ResetPasswordOTP';
+import ResetPassword from './pages/auth/ResetPassword';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen w-full overflow-x-hidden bg-black text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/:serviceId" element={<ServiceDetails />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:blogId" element={<BlogPost />} />
+
+          {/* Auth Routes */}
+          <Route path="/auth/signup" element={<Signup />} />
+          <Route path="/auth/verify-otp" element={<VerifyOTP />} />
+          <Route path="/auth/set-password" element={<SetPassword />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+          <Route path="/auth/reset-password-otp" element={<ResetPasswordOTP />} />
+          <Route path="/auth/reset-password" element={<ResetPassword />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
